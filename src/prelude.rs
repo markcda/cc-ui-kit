689f77b7fee20e2bb8a27c04445ea1867f1cd582
Klimenty Titov <aclo.create@gmail.com>
2025-01-26 01:58:28 +0300

    Bump to `0.1.0-alpha-2`

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,4 +5,49 @@ pub use console_log;
 pub use log;
 
 pub use lucide_leptos;
-pub use thaw;
+pub use thaw::*;
+
+pub fn setup_app(#[allow(unused_variables)] log_level: log::Level, children: Children) {
+  console_error_panic_hook::set_once();
+  #[cfg(debug_assertions)]
+  console_log::init_with_level(log::Level::Debug).unwrap();
+  #[cfg(not(debug_assertions))]
+  console_log::init_with_level(log_level).unwrap();
+  leptos::mount::mount_to_body(move || {
+    view! {
+      <UIApp children />
+    }
+  })
+}
+
+
+
+#[component]
+pub fn UIApp(children: Children) -> impl IntoView {
+  use crate::utils::{cn, light_theme, dark_theme};
+  
+  let preferred_dark = leptos_use::use_preferred_dark();
+  let tw_dark_class = RwSignal::new(if preferred_dark.get() { Some("dark") } else { None });
+  let theme = RwSignal::new({
+    if preferred_dark.get() {
+      dark_theme()
+    } else {
+      light_theme()
+    }
+  });
+  Effect::new(move |_| {
+    theme.set(if preferred_dark.get() {
+      dark_theme()
+    } else {
+      light_theme()
+    })
+  });
+
+  view! {
+    <ConfigProvider theme class=cn("min-h-full", tw_dark_class.get())>
+      <div class="flex flex-col">
+        {children()}
+      </div>
+    </ConfigProvider>
+  }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -5,3 +5,22 @@ pub fn cn(base: &str, additional: Option<&str>) -> String {
     None => base.to_string(),
   }
 }
+
+pub fn dark_theme() -> thaw::Theme {
+  let mut theme = thaw::Theme::dark();
+  theme.color.color_brand_background = "#fafafa".to_string();
+  theme.color.color_brand_background_hover = "#fafafae6".to_string();
+  theme.color.color_brand_background_pressed = "#fafafae6".to_string();
+  theme.color.color_neutral_background_1 = "#08080a".to_string();
+  theme.color.color_neutral_foreground_on_brand = "#18181b".to_string();
+  theme
+}
+
+pub fn light_theme() -> thaw::Theme {
+  let mut theme = thaw::Theme::light();
+  theme.color.color_brand_background = "#17171a".to_string();
+  theme.color.color_brand_background_hover = "#17171ae6".to_string();
+  theme.color.color_brand_background_pressed = "#17171ae6".to_string();
+  theme.color.color_neutral_foreground_on_brand = "#fafafa".to_string();
+  theme
+}
