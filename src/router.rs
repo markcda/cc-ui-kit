0c3f2ad293ac591176809d442429a0694484f953
Klimenty Titov <aclo.create@gmail.com>
2025-01-30 10:13:59 +0300

    Add `redirect` function
    + bump to `0.1.0-alpha-4`

--- src/router.rs
+++ src/router.rs
@@ -1,43 +1,56 @@
 use cc_utils::results::CResult;
 
-/// Получение адреса и порта сервера
+/// Get server's address and port
 pub fn get_host() -> CResult<String> {
   let server_host = web_sys::window()
-    .ok_or::<String>("Не удалось получить параметры окна браузера.".into())?
+    .ok_or::<String>("Can't get browser's window parameters.".into())?
     .document()
-    .ok_or::<String>("Не удалось получить параметры документа браузера.".into())?
+    .ok_or::<String>("Can't get window's document.".into())?
     .location()
-    .ok_or::<String>("Не удалось получить информацию о сервере из браузера.".into())?
+    .ok_or::<String>("Can't get document's location.".into())?
     .host()
-    .map_err(|e| format!("Ошибка получения информации о хосте: {:?}", e))?
+    .map_err(|e| format!("Can't get host: {:?}", e))?
     .to_string();
   Ok(server_host)
 }
 
-/// Получение протокола сервера (HTTP/HTTPS: "http:"/"https:")
+/// Get server protocol (HTTP/HTTPS: "http:"/"https:")
 pub fn get_protocol() -> CResult<String> {
   let server_proto = web_sys::window()
-    .ok_or::<String>("Не удалось получить параметры окна браузера.".into())?
+    .ok_or::<String>("Can't get browser's window parameters.".into())?
     .document()
-    .ok_or::<String>("Не удалось получить параметры документа браузера.".into())?
+    .ok_or::<String>("Can't get window's document.".into())?
     .location()
-    .ok_or::<String>("Не удалось получить информацию о сервере из браузера.".into())?
+    .ok_or::<String>("Can't get document's location.".into())?
     .protocol()
-    .map_err(|e| format!("Ошибка получения информации о протоколе: {:?}", e))?
+    .map_err(|e| format!("Can't get protocol: {:?}", e))?
     .to_string();
   Ok(server_proto)
 }
 
-/// Получение местонахождения
+/// Get path
 pub fn get_path() -> CResult<String> {
   let path = web_sys::window()
-    .ok_or::<String>("Не удалось получить параметры окна браузера.".into())?
+    .ok_or::<String>("Can't get browser's window parameters.".into())?
     .document()
-    .ok_or::<String>("Не удалось получить параметры документа браузера.".into())?
+    .ok_or::<String>("Can't get window's document.".into())?
     .location()
-    .ok_or::<String>("Не удалось получить информацию о сервере из браузера.".into())?
+    .ok_or::<String>("Can't get document's location.".into())?
     .pathname()
-    .map_err(|e| format!("Ошибка получения информации о пути страницы: {:?}", e))?
+    .map_err(|e| format!("Can't get path: {:?}", e))?
     .to_string();
   Ok(path)
+}
+
+/// Redirect to any uri
+pub fn redirect(uri: String) -> CResult<()> {
+  web_sys::window()
+    .ok_or::<String>("Can't get browser's window parameters.".into())?
+    .document()
+    .ok_or::<String>("Can't get window's document.".into())?
+    .location()
+    .ok_or::<String>("Can't get document's location.".into())?
+    .set_href(uri.as_str())
+    .map_err(|e| format!("Can't redirect: {:?}", e))?;
+  Ok(())
 }
